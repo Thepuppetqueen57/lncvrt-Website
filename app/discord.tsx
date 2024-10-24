849f98e4e99a5df4e572f1ff8e6b755d02cbb96b
Lncvrt <lncvrtreal@gmail.com>
2024-10-24 09:15:38 -0700

    Use Websockets (thanks to react-use-lanyard) and fix an issue with spotify

--- app/discord.tsx
+++ app/discord.tsx
@@ -1,113 +1,23 @@
 import React, { useState, useEffect, useRef } from 'react'
 import Image from 'next/image'
 import './discord.css'
-
-interface LanyardResponse {
-  data: LanyardData
-  success: boolean
-}
-
-interface LanyardData {
-  kv: Record<string, any>
-  discord_user: DiscordUser
-  activities: Activity[]
-  discord_status: string
-  active_on_discord_web: boolean
-  active_on_discord_desktop: boolean
-  active_on_discord_mobile: boolean
-  listening_to_spotify: boolean
-  spotify: SpotifyInfo
-}
-
-interface DiscordUser {
-  id: string
-  username: string
-  avatar: string
-  discriminator: string
-  clan: string | null
-  avatar_decoration_data: string | null
-  bot: boolean
-  global_name: string
-  display_name: string
-}
-
-interface Activity {
-  id: string
-  name: string
-  type: number
-  state: string
-  details: string
-  application_id: string
-  timestamps: ActivityTimestamps
-  assets: ActivityAssets
-  created_at: number
-  flags?: number
-  session_id?: string
-  sync_id?: string
-  party?: PartyInfo
-}
-
-interface ActivityTimestamps {
-  start: number
-  end?: number
-}
-
-interface ActivityAssets {
-  large_image: string
-  large_text: string
-  small_image?: string
-  small_text?: string
-}
-
-interface PartyInfo {
-  id: string
-}
-
-interface SpotifyInfo {
-  timestamps: ActivityTimestamps
-  album: string
-  album_art_url: string
-  artist: string
-  song: string
-  track_id: string
-}
+import { useLanyard } from 'react-use-lanyard'
 
 const DiscordCard: React.FC = () => {
-  const [discordStatus, setDiscordStatus] = useState<LanyardResponse>()
-  const hasFetched = useRef(false)
   const [timers, setTimers] = useState<{ [key: string]: number }>({})
 
-  const fetchDiscordStatus = async () => {
-    try {
-      const response = await fetch(
-        'https://api.lanyard.rest/v1/users/1245396407607889954'
-      )
-      const data: LanyardResponse = await response.json()
-
-      if (!response.ok) {
-        throw new Error(
-          data.success ? 'Failed to fetch data' : data.success.toString()
-        )
-      }
-
-      setDiscordStatus(data)
-    } catch (error: any) {}
-  }
-
-  useEffect(() => {
-    if (!hasFetched.current) {
-      fetchDiscordStatus()
-      hasFetched.current = true
-    }
-  }, [])
+  const { loading, status } = useLanyard({
+    userId: '1245396407607889954',
+    socket: true
+  })
 
   useEffect(() => {
     const interval = setInterval(() => {
-      if (discordStatus?.data?.activities) {
+      if (status?.activities) {
         setTimers(prevTimers => {
           const newTimers = { ...prevTimers }
 
-          discordStatus.data.activities.forEach(activity => {
+          status?.activities.forEach(activity => {
             if (activity.timestamps?.start) {
               const startTime = activity.timestamps.start
               const elapsed = Math.floor((Date.now() - startTime) / 1000)
@@ -121,7 +31,7 @@ const DiscordCard: React.FC = () => {
     }, 1000)
 
     return () => clearInterval(interval)
-  }, [discordStatus])
+  }, [status])
 
   const renderActivityType = (type: number) => {
     switch (type) {
@@ -162,8 +72,10 @@ const DiscordCard: React.FC = () => {
 
   const renderActivities = () => {
     if (
-      !discordStatus?.data.activities ||
-      discordStatus.data.activities.length === 0
+      !status?.activities ||
+      status.activities.length === 0 ||
+      (status.activities.length === 1 &&
+        status.activities[0].id === 'spotify:1')
     ) {
       return null
     }
@@ -172,7 +84,7 @@ const DiscordCard: React.FC = () => {
       <div className='activty-card'>
         <div className='activty-title'>Current Activities</div>
         <div className='activty-info mt-[-8px]'>
-          {discordStatus.data.activities
+          {status?.activities
             .filter(activity => activity.id !== 'spotify:1')
             .map(activity => (
               <div key={activity.id} className='activity-item'>
@@ -225,62 +137,61 @@ const DiscordCard: React.FC = () => {
   return (
     <section className='discord-card-section' id='discord-card'>
       <div className='card-content'>
-        <p className='text-red-400 text-sm mb-4'>
-          Note: This is currently WIP and I need to fix issues. This
-          doesn&apos;t refresh automatically either.
-        </p>
-        <div className='icon-and-name'>
-          <Image
-            src='/favicon.png'
-            alt='Icon'
-            className='rounded-[35%] mr-3'
-            width={90}
-            height={90}
-          />
-          <div className='user-info'>
-            <p className='discord-name'>
-              {discordStatus?.data.discord_user.display_name}
-              <span className='text-gray-400 font-extralight ml-2'>
-                (@{discordStatus?.data.discord_user.username})
-              </span>
-            </p>
-            <p className='discord-status'>
-              Status: {capitalizeFirst(discordStatus?.data.discord_status)}
-            </p>
-          </div>
-        </div>
-
-        {discordStatus?.data.listening_to_spotify &&
-          discordStatus.data.spotify && (
-            <div className='activty-card'>
-              <div className='activty-title'>Listening to Spotify</div>
-              <div className='flex items-center gap-4'>
-                <Image
-                  src={`${discordStatus.data.spotify.album_art_url}?size=128`}
-                  alt='Album Art'
-                  width={100}
-                  height={100}
-                  quality={100}
-                  className='album-art'
-                />
-                <div className='spotify-details'>
-                  <p className='spotify-song'>
-                    {discordStatus.data.spotify.song}
-                  </p>
-                  <p className='spotify-artist'>
-                    <span className='text-white'>by</span>{' '}
-                    {discordStatus.data.spotify.artist}
-                  </p>
-                  <p className='spotify-album'>
-                    <span className='text-white'>on</span>{' '}
-                    {discordStatus.data.spotify.album}
-                  </p>
-                </div>
+        {loading ? (
+          <p>Loading...</p>
+        ) : (
+          <>
+            <div className='icon-and-name'>
+              <Image
+                src='/favicon.png'
+                alt='Icon'
+                className='rounded-[35%] mr-3'
+                width={90}
+                height={90}
+              />
+              <div className='user-info'>
+                <p className='discord-name'>
+                  {status?.discord_user.global_name}
+                  <span className='text-gray-400 font-extralight ml-2'>
+                    (@{status?.discord_user.username})
+                  </span>
+                </p>
+                <p className='discord-status'>
+                  Status: {capitalizeFirst(status?.discord_status)}
+                </p>
               </div>
             </div>
-          )}
 
-        {renderActivities()}
+            {status?.listening_to_spotify && status.spotify && (
+              <div className='activty-card'>
+                <div className='activty-title'>Listening to Spotify</div>
+                <div className='flex items-center gap-4'>
+                  <Image
+                    src={`${status.spotify.album_art_url}?size=128`}
+                    alt='Album Art'
+                    width={100}
+                    height={100}
+                    quality={100}
+                    className='album-art'
+                  />
+                  <div className='spotify-details'>
+                    <p className='spotify-song'>{status.spotify.song}</p>
+                    <p className='spotify-artist'>
+                      <span className='text-white'>by</span>{' '}
+                      {status.spotify.artist}
+                    </p>
+                    <p className='spotify-album'>
+                      <span className='text-white'>on</span>{' '}
+                      {status.spotify.album}
+                    </p>
+                  </div>
+                </div>
+              </div>
+            )}
+
+            {renderActivities()}
+          </>
+        )}
       </div>
     </section>
   )
