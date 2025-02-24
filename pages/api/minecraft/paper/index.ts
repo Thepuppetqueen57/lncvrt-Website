718f8b9515e391f082dc5806bf1ddd01cca35d13
Lncvrt <lncvrtreal@gmail.com>
2025-02-24 15:11:28 -0700

    Add helper paths

--- pages/api/minecraft/paper/index.ts
+++ pages/api/minecraft/paper/index.ts
@@ -3,13 +3,7 @@ import { NextApiRequest, NextApiResponse } from 'next';
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
     res.status(200).json({
         "paths": [
-            "folia",
-            "paper",
-            "purformance",
-            "travertine",
-            "vanilla",
-            "velocity",
-            "waterfall"
+            "builds"
         ]
     });
 }
